a0b6e287c3af95ff6bbaf960d373f6d479b5da78
Yusef Codes <youcefcopy@gmail.com>
2026-03-12 13:09:59 +0100

    again, using refular css for responsive font size

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,11 @@
 
 <template>
   <div>
-    <h1 class="text-lg text-red-600">Unlimited movies, TV shows, and more</h1>
-    <h4>Starts at USD 3.99. Cancel anytime.</h4>
-    <h4>Ready to watch? Enter your email to create or restart your membership.</h4>
+    <h1 class="text-red-600">Unlimited movies, TV shows, and more</h1>
+    <h4 class="text-3xl font-bold">Starts at USD 3.99. Cancel anytime.</h4>
+    <h4 class="text-3xl font-bold">
+      Ready to watch? Enter your email to create or restart your membership.
+    </h4>
   </div>
 </template>
 
