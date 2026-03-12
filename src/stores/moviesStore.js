43c8a016dc578f86bb96aaa99709eeb7a2dd0723
Yusef Codes <youcefcopy@gmail.com>
2026-03-12 16:29:37 +0100

    setting up the fetchMovies function

--- src/stores/moviesStore.js
+++ src/stores/moviesStore.js
@@ -1,10 +1,21 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
   // * state
   const movies = ref([])
   const loading = ref(false)
+
+  // * actions
+  const fetchMovies = async () => {
+    loading.value = true
+
+    try {
+    } catch (error) {
+    } finally {
+      // always happens regardless of try/catch blocks
+    }
+  }
 
   return { movies, loading }
 })
