5e1ba6335d2e6e2c8822f8422f5c5a720509f80f
Yusef Codes <youcefcopy@gmail.com>
2026-03-12 12:27:23 +0100

    the conditional rendering is working just perfectly

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import HomeView from '@/views/HomeView.vue'
+import MoviesView from '@/views/MoviesView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
@@ -6,6 +7,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: HomeView,
+  },
+  {
+    path: '/movies',
+    name: 'Movies',
+    component: MoviesView,
   },
 ]
 
