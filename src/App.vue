5b7f32f5cfdd1755bcfe6585aefa45a754114e04
Yusef Codes <youcefcopy@gmail.com>
2026-03-12 12:14:54 +0100

    conditioanl rendering for the hero section

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script setup>
 import NavBar from './components/NavBar.vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
 </script>
 
 <template>
-  <NavBar />
-  <RouterView />
+  <div class="hero-section">
+    <NavBar />
+    <div
+      v-if="route.name === 'Home'"
+      class="hero-section bg-cover bg-center min-h-screen"
+      style="background-image: url('/images/home-bg.jpg')"
+    >
+      <RouterView />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
