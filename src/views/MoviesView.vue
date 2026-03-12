9169dce9d43d00694eea5516dee9ddc1289707ed
Yusef Codes <youcefcopy@gmail.com>
2026-03-12 16:57:20 +0100

    the movies grid layout has been styled

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -11,11 +11,17 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
+  <div class="movies-grid">
     <div v-if="myStore.loading">....Loaaaaading</div>
     <MovieComponent v-for="movie in myStore.movies" :key="movie.title" :movie="movie" />
   </div>
 </template>
 
 <style scoped>
+.movies-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 300px);
+  justify-content: center;
+  gap: 1rem;
+}
 </style>
