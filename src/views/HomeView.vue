50ddf94a2cc048d4f6fb88779d3cc5ef886412ed
Yusef Codes <youcefcopy@gmail.com>
2026-03-12 13:54:36 +0100

    a div for the button and the input filed...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,14 @@
 <template>
   <div class="flex flex-col items-center justify-center flex-1 space-y-3 mt-24">
     <h1 class="font-extrabold text-red-600">Unlimited movies, TV shows, and more</h1>
-    <h4 class="text-3xl font-bold">Starts at USD 3.99. Cancel anytime.</h4>
-    <h4 class="text-3xl font-bold">
+    <h4 class="font-bold">Starts at USD 3.99. Cancel anytime.</h4>
+    <h4 class="font-bold">
       Ready to watch? Enter your email to create or restart your membership.
     </h4>
-    <input type="text" class="py-2 px-6 bg-gray-700 bg-opacity-20" placeholder="Click...." />
+    <div class="flex gap-3 h-14">
+      <input type="text" class="py-2 px-6 bg-opacity-20" placeholder="Click...." />
+      <button>Get Started</button>
+    </div>
   </div>
 </template>
 
