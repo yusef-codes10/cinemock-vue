99d98a18b8b7edbbfc777032200de4c03cff79c5
Yusef Codes <youcefcopy@gmail.com>
2026-03-12 14:10:31 +0100

    responsive hidden navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,7 @@
 <template>
   <nav class="flex justify-around items-center pt-6 pb-1 pr-3 pl-3">
     <h1 class="logo">Cinemcok</h1>
-    <ul class="inline-flex gap-2">
+    <ul class="hidden md:inline-flex gap-4">
       <li>Movies</li>
       <li>Shows</li>
       <li>About</li>
