630121bec5dfa18b805fab245ef00ad038e2da51
Yusef Codes <youcefcopy@gmail.com>
2026-03-12 14:01:44 +0100

    perfect hero section so far

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,10 @@
   <div class="flex flex-col items-center justify-center flex-1 space-y-3 mt-24">
     <h1 class="text-3xl md:text-4xl lg:text-6xl font-extrabold text-red-600">Unlimited movies,</h1>
     <h1 class="text-3xl md:text-4xl lg:text-6xl font-extrabold text-red-600">TV shows, and more</h1>
-    <h4 class="text-xl font-bold">Starts at USD 3.99. Cancel anytime.</h4>
-    <p class="font-thin">Ready to watch? Enter your email to create or restart your membership.</p>
+    <h4 class="text-sm font-bold">Starts at USD 3.99. Cancel anytime.</h4>
+    <p class="text-sm font-thin">
+      Ready to watch? Enter your email to create or restart your membership.
+    </p>
     <div class="flex gap-3 h-14">
       <input type="text" class="py-2 px-6 bg-opacity-20" placeholder="Click...." />
       <button>Get Started</button>
