e2cdd14666afe14e23449b7e2dcab3d02d49baed
Yusef Codes <youcefcopy@gmail.com>
2026-03-13 11:37:09 +0100

    using useRoute to read the url and get in the value with a computed proerty

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,4 +1,13 @@
 <script setup>
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+const route = useRoute()
+
+// better way to access the query is with computed
+const searchQuery = computed(() => {
+  return route.query.q?.toLowerCase() || ''
+})
 </script>
 
 <template>
