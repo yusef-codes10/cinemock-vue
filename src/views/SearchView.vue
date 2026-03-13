3dde31788bae1efad4680f89978ee4e0a50bfe49
Yusef Codes <youcefcopy@gmail.com>
2026-03-13 11:47:51 +0100

    cannot read tolowercase

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,17 +1,26 @@
 <script setup>
 import { useRoute } from 'vue-router'
 import { computed } from 'vue'
+import { moviesStore } from '@/stores/moviesStore.js'
 
 const route = useRoute()
+const myStore = moviesStore()
 
 // better way to access the query is with computed
 const searchQuery = computed(() => {
   return route.query.q?.toLowerCase() || ''
 })
+
+const filterdMovies = computed(() => {
+  return myStore.movies.filter((movie) => movie.title.toLowerCase().includes(searchQuery.value))
+})
 </script>
 
 <template>
-  <div>this is the search view</div>
+  <div>
+    this is the search view
+    {{ filterdMovies['#TITLE'] }}
+  </div>
 </template>
 
 <style scoped>
