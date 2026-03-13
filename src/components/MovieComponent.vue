d547ee6ef2253adc47d3fa902c40c439feb8a067
Yusef Codes <youcefcopy@gmail.com>
2026-03-13 15:50:01 +0100

    adding to the fav array is working yet there are dupes

--- src/components/MovieComponent.vue
+++ src/components/MovieComponent.vue
@@ -1,14 +1,18 @@
 <script setup>
 import { ref } from 'vue'
+import { moviesStore } from '@/stores/moviesStore.js'
 
 defineProps({
   movie: Object,
 })
 
 const isFav = ref(false)
+const myStore = moviesStore()
 
-const addToFavorite = () => {
+const addToFavorite = (movie) => {
   isFav.value = !isFav.value
+  myStore.addToFavList(movie)
+  console.log(myStore.favoriteMovies)
 }
 </script>
 
@@ -21,7 +25,7 @@ const addToFavorite = () => {
         class="w-full h-72 object-cover rounded-lg shadow-lg hover:scale-105 transition-transform duration-300"
       />
       <i
-        @click="addToFavorite"
+        @click="addToFavorite(movie)"
         class="absolute text-3xl top-1 right-0 hover:scale-105 transition-transform duration-300"
         :class="{ 'fa-solid fa-star text-amber-300': isFav, 'fa-regular fa-star': !isFav }"
       ></i>
