2cc7731a3d67178bf73965cc60fd586c9033bfff
Yusef Codes <youcefcopy@gmail.com>
2026-03-13 09:43:06 +0100

    rounded btn

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,13 +2,13 @@
 </script>
 
 <template>
-  <nav class="flex justify-around items-center pt-6 pb-1 pr-3 pl-3">
+  <nav class="flex justify-around items-center pt-6 pb-1 pr-3 pl-3 mb-8">
     <h1 class="logo"><RouterLink :to="{ name: 'Home' }">Cinemock</RouterLink></h1>
     <ul class="hidden md:inline-flex gap-4">
       <li><RouterLink :to="{ name: 'Movies' }">Moives</RouterLink></li>
       <li>Shows</li>
       <li>About</li>
     </ul>
-    <button>Click</button>
+    <button class="rounded-lg">Click</button>
   </nav>
 </template>
