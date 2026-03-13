94e0af72a4ade641615e3ea3e03841b8e8d924c3
Yusef Codes <youcefcopy@gmail.com>
2026-03-13 15:31:53 +0100

    the hover effects look off but it's alright

--- src/components/MovieComponent.vue
+++ src/components/MovieComponent.vue
@@ -6,12 +6,15 @@ defineProps({
 
 <template>
   <div class="movie-card w-48 cursor-pointer">
-    <div class="poster">
+    <div class="poster relative">
       <img
         :src="movie['#IMG_POSTER']"
         :alt="movie['#TITLE']"
         class="w-full h-72 object-cover rounded-lg shadow-lg hover:scale-105 transition-transform duration-300"
       />
+      <i
+        class="fa-regular fa-star absolute text-3xl top-1 right-0 hover:scale-105 transition-transform duration-300"
+      ></i>
     </div>
 
     <div class="title mt-2 text-white text-sm font-semibold text-center">
