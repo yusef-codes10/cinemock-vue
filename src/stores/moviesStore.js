f912aa0892a0a409debe60dd70e1acb86fd877cf
Yusef Codes <youcefcopy@gmail.com>
2026-03-17 12:49:06 +0100

    we have to build our own genre array, since we are only getting ids from the api? does not make any sense

--- src/stores/moviesStore.js
+++ src/stores/moviesStore.js
@@ -10,6 +10,28 @@ export const moviesStore = defineStore('counter', () => {
   // const favoriteMovies = ref([])
   // const search = ref('')
 
+  const GENRES = {
+    28: 'Action',
+    12: 'Adventure',
+    16: 'Animation',
+    35: 'Comedy',
+    80: 'Crime',
+    99: 'Documentary',
+    18: 'Drama',
+    10751: 'Family',
+    14: 'Fantasy',
+    36: 'History',
+    27: 'Horror',
+    10402: 'Music',
+    9648: 'Mystery',
+    10749: 'Romance',
+    878: 'Science Fiction',
+    10770: 'TV Movie',
+    53: 'Thriller',
+    10752: 'War',
+    37: 'Western',
+  }
+
   // * actions
   const fetchAllMovies = async () => {
     // console.log(API_KEY)
@@ -74,7 +96,7 @@ export const moviesStore = defineStore('counter', () => {
         overview: movie.overview,
         release_date: movie.release_date,
         rating: movie.vote_average,
-        genres: movie.genres?.map((g) => g.name) || movie.genre_ids || [],
+        genres: movie.genre_ids.map((id) => GENRES[id]),
         cast: [],
         crew: [],
       }))
