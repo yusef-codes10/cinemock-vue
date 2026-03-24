970ffdb2e38f4cf6770294ba2825e6230edb9e82
Yusef Codes <youcefcopy@gmail.com>
2026-03-24 10:18:18 +0100

    responsive style for thr hero section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,15 +4,15 @@
 <template>
   <!-- ! first hero section -->
   <div class="flex flex-col items-center justify-center flex-1 space-y-3 mt-24">
-    <h1 class="text-3xl md:text-4xl lg:text-5xl font-extrabold">Unlimited movies,</h1>
-    <h1 class="text-3xl md:text-4xl lg:text-5xl font-extrabold">TV shows, and more</h1>
+    <h1 class="text-2xl md:text-4xl lg:text-5xl font-extrabold">Unlimited movies,</h1>
+    <h1 class="text-2xl md:text-4xl lg:text-5xl font-extrabold">TV shows, and more</h1>
     <h4 class="text-sm font-bold">Starts at USD 3.99. Cancel anytime.</h4>
-    <p class="text-sm font-thin">
+    <p class="text-sm font-thin px-2">
       Ready to watch? Enter your email to create or restart your membership.
     </p>
-    <div class="flex gap-3 h-14">
+    <div class="flex flex-col md:flex-row gap-3 h-14">
       <input type="text" class="py-2 px-6 bg-opacity-20" placeholder="Click...." />
-      <button>Get Started</button>
+      <button class="w-fit mx-auto md:w-full">Get Started</button>
     </div>
   </div>
   <!-- ! second trending now -->
