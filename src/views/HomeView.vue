f55c0d33334e6ca7a6bdec216c545c4af24531ab
Yusef Codes <youcefcopy@gmail.com>
2026-03-24 10:26:10 +0100

    style: changed input bg, better responsive design for the input section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,11 +8,17 @@
     <h1 class="text-2xl md:text-4xl lg:text-5xl font-extrabold">TV shows, and more</h1>
     <h4 class="text-sm font-bold">Starts at USD 3.99. Cancel anytime.</h4>
     <p class="text-sm font-thin px-2">
-      Ready to watch? Enter your email to create or restart your membership.
+      Ready to watch? Enter your email to create your membership.
     </p>
     <div class="flex flex-col md:flex-row gap-3 h-14">
-      <input type="text" class="py-2 px-6 bg-opacity-20" placeholder="Click...." />
-      <button class="w-fit mx-auto md:w-full">Get Started</button>
+      <input
+        type="email"
+        class="w-full px-4 py-3 rounded bg-white/20 backdrop-blur outline-none"
+        placeholder="Email"
+      />
+      <button class="w-full sm:w-auto px-6 py-3 bg-red-600 rounded font-semibold">
+        Get Started
+      </button>
     </div>
   </div>
   <!-- ! second trending now -->
