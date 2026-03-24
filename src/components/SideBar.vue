d7c7ecf750806d586673422ee0b7a416fe779860
Yusef Codes <youcefcopy@gmail.com>
2026-03-24 11:21:54 +0100

    sidebar emit, define&trigger -> hide me

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,8 +1,16 @@
 <script setup>
+const emit = defineEmits(['hide'])
+
+const triggerTheEmit = () => {
+  emit('hide')
+}
 </script>
 
 <template>
-  <div></div>
+  <div class="w-60 bg-amber-600 px-4 py-2">
+    side bar
+    <button @click="triggerTheEmit">Emit/hide</button>
+  </div>
 </template>
 
 <style scoped>
